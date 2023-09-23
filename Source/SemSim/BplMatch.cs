14bbf009621ea09aee360cb733695675b72d6208
Jialong-Zhu <2426624752@qq.com>
2023-09-23 15:53:56 +0800

    update BplMatch

---
+++ Source/SemSim/BplMatch.cs
@@ -0,0 +1,329 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Boogie;
+using BType = Microsoft.Boogie.Type;
+using System.Diagnostics;
+
+
+namespace SemSim
+{
+  public class BplMatch
+  {
+    private Utils.VarRenamer _renamer;
+    private int _eqVarsCounter;
+    private int _labelCounter;
+
+    private readonly Variable _havocVar;
+
+    // important: these should not appear in the input programs
+    private static readonly string HavocVarName = "_havoc";
+    private static readonly string AssumeVarPrefix = "_eq";
+    private static readonly string SectionLabelPrefix = "_section";
+    private static readonly string TargetPrefix = "_target";
+
+    private static readonly float ErrorSim = -1;
+
+    private static void Usage()
+    {
+      Console.WriteLine("bplmatch - find the best matching for given query and target.");
+      Console.WriteLine("Usage: bplmatch <query.bpl> <target.bpl>");
+      Console.WriteLine("-break - start in debug mode");
+    }
+
+    static int Main(string[] args)
+    {
+
+      if (args.Length < 2 || args.Length > 3)
+      {
+        Usage();
+        return -1;
+      }
+
+      if (args.Length == 3 && args[2].ToLower() == "-break")
+      {
+        Debugger.Launch();
+      }
+
+      using StreamReader qr = new StreamReader(args[0]), tr = new StreamReader(args[1]);
+      float sim = RunMatch(qr.ReadToEnd(), tr.ReadToEnd());
+      Console.Out.WriteLine($"Sim: {sim}");
+
+      return 0;
+    }
+
+    public static float RunMatch(string queryText, string targetText)
+    {
+      return (new BplMatch()).Run(queryText, targetText);
+    }
+
+    public BplMatch()
+    {
+      _renamer = new Utils.VarRenamer(TargetPrefix + ".", new string[] { });
+      _eqVarsCounter = 0;
+      _havocVar = new LocalVariable(Token.NoToken, new TypedIdent(Token.NoToken, HavocVarName, BType.Bool));
+    }
+
+    private float Run(string queryText, string targetText)
+    {
+      Program queryProgram, targetProgram;
+      if (!Utils.ParseProgram(queryText, out queryProgram) || !Utils.ParseProgram(targetText, out targetProgram))
+      {
+        return ErrorSim;
+      }
+
+      if (queryProgram.Implementations.Count() != 1 || targetProgram.Implementations.Count() != 1)
+      {
+        Debug.WriteLine("One Implementation per program, please.");
+        return ErrorSim;
+      }
+
+
+      JoinTopLevelDeclarations(queryProgram, targetProgram);
+
+      var queryImplementation = queryProgram.Implementations.Single();
+      var targetImplementation = targetProgram.Implementations.Single();
+
+      int numTotalLocals = queryImplementation.LocVars.Count + targetImplementation.LocVars.Count;
+
+      targetImplementation = _renamer.VisitImplementation(targetImplementation);
+
+      int numAssert;
+      var assumeVars = new List<Tuple<Variable, Expr, Expr>>();
+      var blocks = CreateAssertsBlocks(queryImplementation, targetImplementation, assumeVars, out numAssert);
+
+      if (numAssert > Utils.MaxAsserts)
+      {
+        Debug.WriteLine($"Max asserts exceeded: {numAssert} > {Utils.MaxAsserts}");
+        return ErrorSim;
+      }
+
+      JoinImplementations(queryImplementation, targetImplementation);
+
+      queryImplementation.Blocks.Last().TransferCmd = new GotoCmd(Token.NoToken, blocks);
+      queryImplementation.Blocks.AddRange(blocks);
+
+      // print new program and reparse to fix resolving problems
+      string joinedText = Utils.PrintProgram(queryProgram);
+      if (!Utils.ParseProgram(joinedText, out queryProgram))
+      {
+        return ErrorSim;
+      }
+
+      // run Boogie and get the output
+      var output = Utils.RunBoogie(queryProgram);
+      if (output == null)
+      {
+        return ErrorSim;
+      }
+
+      // reparse queryProgram from joinedText to avoid side effects from RunBoogie
+      if (!Utils.ParseProgram(joinedText, out queryProgram))
+      {
+        return ErrorSim;
+      }
+
+      // find all the failed asserts 
+      var failedAssertsLineNumbers = new HashSet<int>();
+      var match = Regex.Match(output, @"\(([0-9]+),[0-9]+\): Error: this assertion could not be proved");
+      while (match.Success)
+      {
+        failedAssertsLineNumbers.Add(int.Parse(match.Groups[1].Value));
+        match = match.NextMatch();
+      }
+
+      // map blocks to true asserts
+      var trueAsserts = new Dictionary<Block, HashSet<AssertCmd>>();
+      queryProgram.Implementations.Single().Blocks.ForEach(b =>
+      {
+        trueAsserts[b] = new HashSet<AssertCmd>();
+        b.Cmds.ForEach(c =>
+        {
+          var ac = c as AssertCmd;
+          if (ac != null && !failedAssertsLineNumbers.Contains(ac.Line))
+            trueAsserts[b].Add(ac);
+        });
+      });
+
+      // extract the assert expressions and add in the assume expressions
+      var bestBlock = trueAsserts.Keys.First(b => trueAsserts[b].Count == trueAsserts.Max(p => p.Value.Count));
+      bestBlock.Cmds.ForEach(c =>
+      {
+        var ac = c as AssumeCmd;
+        if (ac != null)
+        {
+          assumeVars.ForEach(t =>
+          {
+            if (t.Item1.ToString() == ac.Expr.ToString())
+            {
+              Debug.WriteLine(t.Item2 + " == " + t.Item3);
+            }
+          });
+
+        }
+      });
+      Debug.WriteLine("==> ");
+
+      bool error = false;
+      var matchedVars = new HashSet<string>(); // remember that a variable can be matched more than once
+      trueAsserts[bestBlock].ForEach(a =>
+      {
+        var s = a.Expr.ToString().Split(new string[] { " || " }, StringSplitOptions.None);
+        if (s.Count() <= 1)
+        {
+          error = true;
+          Debug.WriteLine($"Too few exprs from {a.Expr}");
+        }
+        else
+        {
+          Debug.WriteLine(s[1]);
+          var vars = s[1].Split(new string[] { " == " }, StringSplitOptions.None);
+          matchedVars.Add(vars[0]);
+          matchedVars.Add(vars[1]);
+        }
+      });
+      if (error)
+      {
+        return ErrorSim;
+      }
+
+      float res = matchedVars.Count / (float)numTotalLocals;
+      Debug.WriteLine($"\nMatched sim: {res}");
+      return res;
+    }
+
+    private void JoinImplementations(Implementation queryImplementation, Implementation targetImplementation)
+    {
+      queryImplementation.InParams.AddRange(targetImplementation.InParams);
+      queryImplementation.Proc.InParams.AddRange(targetImplementation.InParams);
+      queryImplementation.LocVars.AddRange(targetImplementation.LocVars);
+      queryImplementation.LocVars.Add(_havocVar);
+      queryImplementation.Blocks.Last().TransferCmd = new GotoCmd(Token.NoToken,
+          new List<Block>() { targetImplementation.Blocks.First() });
+      queryImplementation.Blocks.AddRange(targetImplementation.Blocks);
+    }
+
+    private void JoinTopLevelDeclarations(Program queryProgram, Program targetProgram)
+    {
+      // add all of target's functions, constants, globals and procedures to query
+      queryProgram.AddTopLevelDeclarations(targetProgram.Functions.Where(f2 => !queryProgram.Functions.Select(f => f.Name).Contains(f2.Name)));
+      queryProgram.AddTopLevelDeclarations(targetProgram.Constants.Select(c2 => new Constant(Token.NoToken, new TypedIdent(Token.NoToken, TargetPrefix + c2.Name, c2.TypedIdent.Type), c2.Unique)));
+      queryProgram.AddTopLevelDeclarations(targetProgram.GlobalVariables.Select(g2 => new GlobalVariable(Token.NoToken, new TypedIdent(Token.NoToken, TargetPrefix + g2.Name, g2.TypedIdent.Type))));
+    }
+
+    private List<Expr> CreateAssertsExprs(Implementation queryImplementation, Implementation targetImplementation)
+    {
+      var result = new List<Expr>();
+      queryImplementation.LocVars.ForEach(v =>
+      {
+        var type = Utils.GetExprType(Expr.Ident(v));
+        if (type != null)
+          targetImplementation.LocVars.ForEach(v2 =>
+          {
+            if (type.Equals(Utils.GetExprType(Expr.Ident(v2)))) result.Add(Expr.Eq(Expr.Ident(v), Expr.Ident(v2)));
+          });
+      });
+      return result;
+    }
+
+    private List<Block> CreateAssertsBlocks(Implementation queryImplementation, Implementation targetImplementation, List<Tuple<Variable, Expr, Expr>> assumeVars, out int numAsserts)
+    {
+      var exprs = CreateAssertsExprs(queryImplementation, targetImplementation);
+      var assertsCmds = CreateAsserts(exprs);
+      numAsserts = assertsCmds.Count;
+
+      queryImplementation.InParams.ForEach(iq =>
+      {
+        targetImplementation.InParams.ForEach(it =>
+        {
+          if (Equals(iq.TypedIdent.Type, it.TypedIdent.Type))
+          {
+            var eqVar = new LocalVariable(Token.NoToken, new TypedIdent(Token.NoToken, AssumeVarPrefix + "_" + _eqVarsCounter++, BType.Bool));
+            assumeVars.Add(new Tuple<Variable, Expr, Expr>(eqVar, Expr.Ident(iq), Expr.Ident(it)));
+            queryImplementation.Blocks[0].Cmds.Insert(0, Utils.CreateAssignCmd(new List<IdentifierExpr>() { Expr.Ident(eqVar) }, new List<Expr>() { Expr.Eq(Expr.Ident(iq), Expr.Ident(it)) }));
+          }
+        });
+      });
+
+      // The equality vars are grouped according to lhs. This means that expressions like eq_0 = `rax == v2.rcx` and eq_13 = `rax == v2.p0` will be 
+      // grouped together, to make the assumes choosing algorithm more efficient (we won't select eq_0 and eq_13 for the same section)
+      var eqVarsGroups = new Dictionary<string, List<Tuple<Variable, Expr, Expr>>>();
+      assumeVars.ForEach(t =>
+      {
+        var lhs = t.Item2.ToString();
+        if (!eqVarsGroups.ContainsKey(lhs))
+          eqVarsGroups[lhs] = new List<Tuple<Variable, Expr, Expr>>();
+        eqVarsGroups[lhs].Add(t);
+      });
+      assumeVars.ForEach(t => queryImplementation.LocVars.Add(t.Item1));
+      return CreateAssertsWithAssumes(eqVarsGroups.Values.ToList(), assertsCmds);
+    }
+
+    private List<Block> CreateAssertsWithAssumes(List<List<Tuple<Variable, Expr, Expr>>> eqVarsGroups, List<Cmd> asserts)
+    {
+      int n = eqVarsGroups.Count();
+      if (n == 0)
+      {
+        var b = new Block { Label = SectionLabelPrefix + "_" + _labelCounter++ };
+        b.Cmds.AddRange(asserts);
+        return new List<Block>() { b };
+      }
+
+      var result = new List<Block>();
+      EnumerateAssumes(new List<Tuple<int, int>>(), n, asserts, eqVarsGroups, result);
+      return result;
+    }
+
+    private List<Cmd> CreateAsserts(IEnumerable<Expr> exprs)
+    {
+      var result = new List<Cmd>();
+      foreach (var e in exprs)
+      {
+        result.Add(new HavocCmd(Token.NoToken, new List<IdentifierExpr>() { Expr.Ident(_havocVar) }));
+        result.Add(new AssertCmd(Token.NoToken, Expr.Or(Expr.Ident(_havocVar), e)));
+      }
+      // if the tracelets have conflicting assumes, everything can be proved.
+      // add an 'assert false;' at the end, to check at this did not happen
+      result.Add(new AssertCmd(Token.NoToken, Expr.False));
+      return result.ToList();
+    }
+
+    private void EnumerateAssumes(List<Tuple<int, int>> pick, int depth, List<Cmd> asserts,
+        List<List<Tuple<Variable, Expr, Expr>>> eqVarsGroups, List<Block> result)
+    {
+      if (depth == 0)
+      {
+        var usedExprs = new HashSet<string>();
+        var eqVarsPick = new List<Variable>();
+        foreach (Tuple<int, int> t in pick)
+        {
+          var tuple = eqVarsGroups[t.Item1][t.Item2];
+          if (usedExprs.Contains(tuple.Item3.ToString()))
+            return;
+          usedExprs.Add(tuple.Item3.ToString());
+          eqVarsPick.Add(tuple.Item1);
+        }
+
+        var b = new Block { Label = SectionLabelPrefix + "_" + _labelCounter++ };
+        foreach (var eqv in eqVarsPick)
+          b.Cmds.Add(new AssumeCmd(Token.NoToken, Expr.Ident(eqv)));
+        b.Cmds.AddRange(asserts);
+        b.TransferCmd = new ReturnCmd(Token.NoToken);
+        result.Add(b);
+        return;
+      }
+
+      // always start from the next group
+      for (var i = (pick.Count > 0) ? pick.Last().Item1 + 1 : 0; i < eqVarsGroups.Count; ++i)
+      {
+        // inside the group, try all candidates
+        for (int j = 0; j < eqVarsGroups[i].Count; ++j)
+        {
+          var l = new List<Tuple<int, int>>(pick) { new Tuple<int, int>(i, j) };
+          EnumerateAssumes(l, depth - 1, asserts, eqVarsGroups, result);
+        }
+      }
+
+    }
+
+  }
+
+}
---
+++ Source/SemSim/Utils.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.Boogie;
+using System.Diagnostics;
+
+namespace SemSim
+{
+  public class Utils
+  {
+    public static readonly int MaxAsserts = 10000;
+
+    private static ExecutionEngine Engine;
+    private static CommandLineOptions PrintOptions;
+    private static CommandLineOptions ProcessOptions;
+    private static StringWriter PrintBuffer;
+    private static StringWriter ProcessBuffer;
+
+    static Utils()
+    {
+      PrintBuffer = new StringWriter();
+      ProcessBuffer = new StringWriter();
+
+      var printBoogieOptions = $"/typeEncoding:m -timeLimit:1 -removeEmptyBlocks:0 /printInstrumented";
+      PrintOptions = CommandLineOptions.FromArguments(TextWriter.Null, printBoogieOptions.Split(' '));
+      PrintOptions.UseUnsatCoreForContractInfer = true;
+      PrintOptions.ContractInfer = true;
+      PrintOptions.ExplainHoudini = true;
+
+      var processBoogieOptions = $"/timeLimit:1 /errorLimit:{MaxAsserts} /useArrayAxioms";
+      ProcessOptions = CommandLineOptions.FromArguments(TextWriter.Null, processBoogieOptions.Split(' '));
+
+      Engine = ExecutionEngine.CreateWithoutSharedCache(ProcessOptions);
+    }
+
+    public static bool ParseProgram(string text, out Program program)
+    {
+      program = null;
+      int errCount;
+      try
+      {
+        errCount = Parser.Parse(text, "from_string", out program);
+        if (errCount != 0 || program == null)
+        {
+          Debug.WriteLine($"Parse errors detected in: {text}");
+          return false;
+        }
+      }
+      catch (Exception e)
+      {
+        Debug.WriteLine($"Exception parsing program: {e.Message}");
+        return false;
+      }
+
+      errCount = program.Resolve(PrintOptions);
+      if (errCount > 0)
+      {
+        Debug.WriteLine($"Name resolution errors in: {text}");
+        return false;
+      }
+
+      ModSetCollector c = new ModSetCollector(PrintOptions);
+      c.DoModSetAnalysis(program);
+
+      return true;
+    }
+
+    public static string PrintProgram(Program program)
+    {
+      var ttw = new TokenTextWriter(ProcessBuffer, PrintOptions);
+      program.Emit(ttw);
+
+      string res = ProcessBuffer.ToString();
+      ProcessBuffer.GetStringBuilder().Clear();
+      return res;
+    }
+
+    public static string? RunBoogie(Program program)
+    {
+      var success = Engine.ProcessProgram(ProcessBuffer, program, "from_string").Result;
+
+      if (!success)
+      {
+        Debug.WriteLine($"Boogie running errors due to: {ProcessBuffer}");
+      }
+
+      string? res = success ? ProcessBuffer.ToString() : null;
+      ProcessBuffer.GetStringBuilder().Clear();
+      return res;
+    }
+
+    public class VarRenamer : StandardVisitor
+    {
+      private readonly string _prefix;
+      public List<string> Ignore;
+      public VarRenamer(string prefix, string[] ignore)
+      {
+        _prefix = prefix;
+        Ignore = new List<string>(ignore);
+      }
+
+      public override Implementation VisitImplementation(Implementation node)
+      {
+        var result = base.VisitImplementation(node);
+        result.Name = _prefix + result.Name;
+        result.InParams = result.InParams.Select(i => new LocalVariable(Token.NoToken, new TypedIdent(Token.NoToken, i.Name, i.TypedIdent.Type)) as Variable).ToList();
+        result.OutParams = result.OutParams.Select(o => new LocalVariable(Token.NoToken, new TypedIdent(Token.NoToken, o.Name, o.TypedIdent.Type)) as Variable).ToList();
+        result.LocVars = result.LocVars.Select(v => new LocalVariable(Token.NoToken, new TypedIdent(Token.NoToken, v.Name, v.TypedIdent.Type)) as Variable).ToList();
+        return result;
+      }
+
+      public override Expr VisitNAryExpr(NAryExpr node)
+      {
+        node.Args = node.Args.Select(arg => VisitExpr(arg)).ToList();
+        return node;
+      }
+
+      public override Variable VisitVariable(Variable node)
+      {
+        if (node.Name.StartsWith(_prefix))
+          return node;
+        var result = node.Clone() as Variable;
+        if (result == null)
+          return node;
+        result.Name = _prefix + result.Name;
+        return result;
+      }
+
+      public override Expr VisitIdentifierExpr(IdentifierExpr node)
+      {
+        var result = new IdentifierExpr(Token.NoToken, VisitVariable(node.Decl));
+        return result;
+      }
+
+      public override Cmd VisitCallCmd(CallCmd node)
+      {
+        if (!Ignore.Any(p => node.callee.StartsWith(p)))
+          node.callee = _prefix + node.callee;
+        return base.VisitCallCmd(node);
+      }
+
+      public override Cmd VisitAssignCmd(AssignCmd node)
+      {
+        var renamedLhss = new List<AssignLhs>();
+        foreach (var l in node.Lhss)
+        {
+          if (l is SimpleAssignLhs)
+          {
+            renamedLhss.Add(new SimpleAssignLhs(Token.NoToken, Expr.Ident(VisitVariable(l.DeepAssignedVariable))));
+          }
+          else if (l is MapAssignLhs)
+          {
+            var mal = l as MapAssignLhs;
+            renamedLhss.Add(new MapAssignLhs(Token.NoToken,
+                new SimpleAssignLhs(Token.NoToken, VisitIdentifierExpr(mal.DeepAssignedIdentifier) as IdentifierExpr),
+                mal.Indexes.Select(i => VisitExpr(i)).ToList()));
+          }
+          else
+          {
+            Debug.WriteLine($"Unknown assign type: {l.GetType()}");
+          }
+        }
+
+        var renamedRhss = new List<Expr>();
+        node.Rhss.ForEach(r => renamedRhss.Add(VisitExpr(r)));
+
+        var result = new AssignCmd(Token.NoToken, renamedLhss, renamedRhss);
+        return result;
+      }
+
+      public override Block VisitBlock(Block node)
+      {
+        node.Label = _prefix + node.Label;
+        var gotoCmd = node.TransferCmd as GotoCmd;
+        if (gotoCmd != null)
+          node.TransferCmd = new GotoCmd(Token.NoToken, gotoCmd.labelNames.Select(l => _prefix + l).ToList());
+        return base.VisitBlock(node);
+      }
+    }
+
+    public static string GetExprType(Expr expr)
+    {
+      var le = expr as LiteralExpr;
+      if (le != null)
+        return le.Type.ToString();
+      var ie = expr as IdentifierExpr;
+      if (ie != null)
+        return ie.Decl.TypedIdent.Type.ToString();
+      var ne = expr as NAryExpr;
+      if (ne != null && ne.Fun is MapSelect)
+        return ((ne.Args[0] as IdentifierExpr).Decl.TypedIdent.Type as MapType).Result.ToString();
+      return null;
+    }
+
+    public static AssignCmd CreateAssignCmd(IEnumerable<IdentifierExpr> lhs, IEnumerable<Expr> rhs)
+    {
+      List<AssignLhs> assignLhss = new List<AssignLhs>();
+      lhs.ForEach(i => assignLhss.Add(new SimpleAssignLhs(Token.NoToken, i)));
+      return new AssignCmd(new Token(), assignLhss, rhs.ToList());
+    }
+  }
+}
